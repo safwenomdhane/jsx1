30a73d7850a3486ee10bdd847677d7805d6c126a
safwenomdhane <85678154+safwenomdhane@users.noreply.github.com>
2021-09-17 00:23:21 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,336 @@
+import logo1 from './assets/logo.png'
+import Sun from './sun.svg'
+import Water from './water.svg'
+import monstera from './monstera.jpg'
+import lyrata from './lyrata.jpg'
+import pothos from './pothos.jpg'
+import succulent from './succulent.jpg'
+import olivier from './olivier.jpg'
+import basil from './basil.jpg'
+import mint from './mint.jpg'
+import calathea from './calathea.jpg'
+import cactus from './cactus.jpg'
+import './App.css'
+import { useState, useEffect } from 'react'
+
+function Banner()  {
+  const title = 'La maison jungle'
+  return (
+      <div className='lmj-banner'>
+          <img src={logo1} alt='La maison jungle' className='lmj-logo' />
+          <h1 className='lmj-title'>{title}</h1>
+      </div>
+  )
+}
+//plant list
+const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		light: 2,
+		water: 3,
+		cover: monstera,
+		price: 15
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: lyrata,
+		price: 16
+	},
+
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: pothos,
+
+		price: 9
+	},
+	{
+		name: 'calathea',
+		category: 'classique',
+		id: '4kk',
+		light: 2,
+		water: 3,
+		cover: calathea,
+
+		price: 20
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: olivier,
+		price: 25
+	},
+
+	{
+		name: 'cactus',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: cactus,
+		price: 6
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		light: 2,
+		water: 3,
+		cover: basil,
+		price: 5
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: succulent,
+		price: 8
+	},
+
+	{
+		name: 'menthe',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: mint,
+		price: 4
+	},
+	
+]
+
+
+const quantityLabel = {
+	1: 'peu',
+	2: 'modérément',
+	3: 'beaucoup'
+}
+
+function CareScale({ scaleValue, careType }) {
+	const range = [1, 2, 3]
+	const scaleType =
+		careType === 'light' ? (
+			<img src={Sun} alt='sun-icon' />
+		) : (
+			<img src={Water} alt='water-icon' />
+		)
+
+	return (
+		<div
+			onClick={() =>
+				alert(
+					`Cette plante requiert ${quantityLabel[scaleValue]} ${
+						careType === 'light' ? 'de lumière' : "d'arrosage"
+					}`
+				)
+			}
+		>
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+		</div>
+	)
+}
+
+function Cart({ cart, updateCart }) {
+	const [isOpen, setIsOpen] = useState(true)
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
+	)
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	}, [total])
+
+	return isOpen ? (
+		<div className='lmj-cart'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
+						))}
+					</ul>
+					<h3>Total :{total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
+		</div>
+	) : (
+		<div className='lmj-cart-closed'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(true)}
+			>
+				Ouvrir le Panier
+			</button>
+		</div>
+	)
+}
+
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light, price }) {
+	return (
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<span className='lmj-plant-item-price'>{price}€</span>
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+			{name}
+			<div>
+				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='light' scaleValue={light} />
+			</div>
+		</li>
+	)
+}
+
+function ShoppingList({ cart, updateCart }) {
+	const [activeCategory, setActiveCategory] = useState('')
+	const categories = plantList.reduce(
+		(acc, elem) =>
+			acc.includes(elem.category) ? acc : acc.concat(elem.category),
+		[]
+	)
+
+	function addToCart(name, price) {
+		const currentPlantAdded = cart.find((plant) => plant.name === name)
+		if (currentPlantAdded) {
+			const cartFilteredCurrentPlant = cart.filter(
+				(plant) => plant.name !== name
+			)
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantAdded.amount + 1 }
+			])
+		} else {
+			updateCart([...cart, { name, price, amount: 1 }])
+		}
+	}
+
+	return (
+		<div className='lmj-shopping-list'>
+			<Categories
+				categories={categories}
+				setActiveCategory={setActiveCategory}
+				activeCategory={activeCategory}
+			/>
+
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light, price, category }) =>
+					!activeCategory || activeCategory === category ? (
+						<div key={id}>
+							<PlantItem
+								cover={cover}
+								name={name}
+								water={water}
+								light={light}
+								price={price}
+							/>
+							<button onClick={() => addToCart(name, price)}>Ajouter</button>
+						</div>
+					) : null
+				)}
+			</ul>
+		</div>
+	)
+}
+
+  
+function Categories({ setActiveCategory, categories, activeCategory }) {
+	return (
+		<div className='lmj-categories'>
+			<select
+				value={activeCategory}
+				onChange={(e) => setActiveCategory(e.target.value)}
+				className='lmj-categories-select'
+			>
+				<option value=''>---</option>
+				{categories.map((cat) => (
+					<option key={cat} value={cat}>
+						{cat}
+					</option>
+				))}
+			</select>
+			<button onClick={() => setActiveCategory('')}>Réinitialiser</button>
+		</div>
+	)
+}
+
+function Footer() {
+	const [inputValue, setInputValue] = useState('')
+
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
+
+	return (
+		<footer className='lmj-footer'>
+			<div className='lmj-footer-elem'>
+				Pour les passionné·e·s de plantes 🌿🌱🌵
+			</div>
+			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
+		</footer>
+	)
+}
+function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
+	return (
+		<div>
+			<Banner />
+					<div className='lmj-layout-inner'>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
+			</div>
+			<Footer />
+		</div>
+	)
+}
+
+
+export default App;
---
+++ src/Banner.js
@@ -0,0 +1,5 @@
+import '../styles/Banner.css'
+<div className='lmj-banner'>
+    <h1>La maison jungle</h1>
+</div>
+
